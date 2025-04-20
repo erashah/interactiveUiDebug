eae665b69b775431c68e9621b0416d9f5bc58cdf
Era Shah <erashah001@gmail.com>
2025-04-20 12:27:18 -0400

    Initial Commit

---
+++ interactiveUiDebug/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  interactiveUiDebug
+//
+//  Created by Era Shah on 4/20/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ interactiveUiDebug/interactiveUiDebugApp.swift
@@ -0,0 +1,17 @@
+//
+//  interactiveUiDebugApp.swift
+//  interactiveUiDebug
+//
+//  Created by Era Shah on 4/20/25.
+//
+
+import SwiftUI
+
+@main
+struct interactiveUiDebugApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
